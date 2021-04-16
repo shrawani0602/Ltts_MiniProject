b9f907d34d8fc82a68546840476e69aaebe749cd
shrawani0602 <49336914+shrawani0602@users.noreply.github.com>
2021-04-16 15:24:29 +0530

    Create testderegister.c

---
+++ 3_Implementation/test/testderegister.c
@@ -0,0 +1,35 @@
+#include "unity.h"
+#include <fun.h>
+
+/* Modify these two lines according to the project */
+#define PROJECT_NAME    "BANKING SYSTEM"
+
+/* Prototypes for all the test functions */
+void test_deregister(void);
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+/* Start of the application test */
+int main()
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_deregister);
+  
+
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
+
+/* Write all the test functions */ 
+void test_deregister(void); {
+  TEST_ASSERT_EQUAL(1);
+  
+  /* Dummy fail*/
+  
+}
